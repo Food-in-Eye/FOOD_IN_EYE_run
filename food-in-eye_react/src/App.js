814d15c06b28123f1222b7d2d8c2201cb1e32e9d
seo <deergirl11@gmail.com>
2023-02-28 02:23:22 +0900

    add routes for new pages

--- food-in-eye_react/src/App.js
+++ food-in-eye_react/src/App.js
@@ -1,7 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminPage from "./pages/AdminPage";
+import AdminManagePage from "./pages/AdminManagePage";
+import DetailAnalysisPage from "./pages/DetailAnalysisPage";
 import AnalysisPage from "./pages/AnalysisPage";
-import ManagePage from "./pages/ManagePage";
+import BenefitManagePage from "./pages/BenefitManagePage";
+import StoreManagePage from "./pages/StoreManagePage";
+import OrderManagePage from "./pages/OrderManagePage";
+import MenuManagePage from "./pages/MenuManagePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
@@ -10,9 +14,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin" element={<AdminManagePage />} />
+        <Route path="/detail" element={<DetailAnalysisPage />} />
         <Route path="/analysis" element={<AnalysisPage />} />
-        <Route path="/manage" element={<ManagePage />} />
+        <Route path="/benefit-manage" element={<BenefitManagePage />} />
+        <Route path="/store-manage" element={<StoreManagePage />} />
+        <Route path="/order-manage" element={<OrderManagePage />} />
+        <Route path="/menu-manage" element={<MenuManagePage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<LoginPage />} />
