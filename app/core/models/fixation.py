ebcea497e13cfeac8a5ab4ed2ac6853ed49d62d9
JIIN <j00j12i11@duksung.ac.kr>
2023-03-04 00:38:49 +0900

    Correct typos

--- app/core/models/fixation.py
+++ app/core/models/fixation.py
@@ -3,8 +3,8 @@ from .gaze_point import GazePointModel
 
 
 class FixationModel(BaseModel):
-    cy: float = Field(title="center x of fixation")
-    cx: float = Field(title="center y of fixation")
+    cx: float = Field(title="center x of fixation")
+    cy: float = Field(title="center y of fixation")
     st: int = Field(title="start time of fixation")
     et: int = Field(title="end time of fixation")
     r: float = Field(title="radius of fixation")
