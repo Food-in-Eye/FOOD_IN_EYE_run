e2948a281a7183bef85bf3ebcb474e2531a38c7b
seo <deergirl11@gmail.com>
2023-03-07 19:50:33 +0900

    add dropmenu function in menubar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -5,26 +5,60 @@ function MenuBar() {
   return (
     <section className={style.header}>
       <h2>LOGO</h2>
-      <ul className={style.gnb}>
-        <li>
-          <a href="./main">Home</a>
-        </li>
-        <li>
-          <a href="./menu-manage">관리</a>
-        </li>
-        <li>
-          <a href="./analysis">분석</a>
-        </li>
-        <li>
-          <a href="./admin">설정</a>
-        </li>
-      </ul>
+      <div className={style.dropmenu}>
+        <ul>
+          <li>
+            <a href="./main" className={style.menubar_head}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#!" className={style.menubar_head}>
+              관리
+            </a>
+            <ul>
+              <li>
+                <a href="./store-manage">가게 관리</a>
+              </li>
+              <li>
+                <a href="./menu-manage">메뉴 관리</a>
+              </li>
+              <li>
+                <a href="./order-manage">주문 관리</a>
+              </li>
+              <li>
+                <a href="./layout-manage">메뉴판 배치</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#!" className={style.menubar_head}>
+              분석
+            </a>
+            <ul>
+              <li>
+                <a href="./analysis">전체 메뉴분석</a>
+              </li>
+              <li>
+                <a href="./analysis-detail">메뉴별 분석</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="./admin" className={style.menubar_head}>
+              설정
+            </a>
+          </li>
+        </ul>
+      </div>
 
-      <ul className={style.util}>
-        <li>
-          <a href="/">Logout</a>
-        </li>
-      </ul>
+      <div className="logout">
+        <ul className={style.util}>
+          <li>
+            <a href="/">Logout</a>
+          </li>
+        </ul>
+      </div>
 
       <div className={style.profile}>
         <img src={require("../images/profile.jpeg")} alt="프로필 이미지"></img>
