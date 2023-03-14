3f6759eeab3acd8e0d64743d3e465a62ac1eb4d3
seo <deergirl11@gmail.com>
2023-03-14 21:57:09 +0900

    add store manage page components(진행중)

--- src/pages/StoreManagePage.js
+++ src/pages/StoreManagePage.js
@@ -1,5 +1,6 @@
 import MenuBar from "../components/MenuBar";
 import Store from "../css/StoreManage.module.css";
+import Button from "../css/Button.module.css";
 
 function StoreManagePage() {
   return (
@@ -8,7 +9,29 @@ function StoreManagePage() {
         <MenuBar />
       </section>
       <div className={Store.inner}>
-        <p>가게 관리 페이지</p>
+        <section className="status-bar">
+          <div className={Store.status}>
+            <button>영업 중</button>
+            <button>영업 종료</button>
+          </div>
+        </section>
+        <section className="description">
+          <h1>000 가게</h1>
+          <div>
+            <ul>
+              <h3>가게 한줄 소개</h3>
+              <li></li>
+            </ul>
+            <button className={Button.modify}>수정</button>
+          </div>
+        </section>
+        <section className="notice">
+          <div>
+            <h3>가게 공지사항</h3>
+            <button className={Button.modify}>수정</button>
+            <button className={Button.modify}>추가</button>
+          </div>
+        </section>
       </div>
     </div>
   );
