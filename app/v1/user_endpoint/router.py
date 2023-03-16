7e6ce084e24de87078314bee8d5e0efddf05a935
JIIN <j00j12i11@duksung.ac.kr>
2023-03-17 00:55:34 +0900

    set function temporarily for testing

--- app/v1/user_endpoint/router.py
+++ app/v1/user_endpoint/router.py
@@ -10,3 +10,32 @@ router = APIRouter(prefix="/user", tags=["Android"])
 @router.get("/hi")
 async def hello():
     return {"message": "Hello 'api/v1/user/hi'"}
+
+@router.get('/api/v1/user/stores')
+async def get_store_list():
+    """ test 할 수 있도록 임시로 추가 """
+    return {
+	'request': 'api/v1/user/stores',
+	'response': [
+		{
+		    "_id": "640f314539dec4c7ae2cad9e",
+		    "name": "니나노덮밥",
+		    "description": "맛있는 함박오므라이스와 카레라이스를 팝니다~!",
+		    "schedule": "9시~18시 영업, 월요일 휴무",
+			"notice": None,
+			"status": 1,
+			"img_url": None,
+			"m_id": "640f314539dec4c11e2c24d9e"
+		},
+		{
+		    "_id": "640f30d62f922537bd16c4fa",
+		    "name": "파스타",
+		    "description": "다양한 양식 음식이 있습니다.",
+		    "schedule": "9시~18시 영업, 주말 제외",
+			"notice": "개인사정으로 다음주 화요일까지 잠시 휴업합니다.",
+			"status": 2,
+			"img_url": None,
+			"m_id": "640f31451242144c11e2c24d9e"
+		}
+	]
+}
