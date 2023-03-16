9abd08576b0ace6740d7ffad337538fbd1b90cc9
JIIN <j00j12i11@duksung.ac.kr>
2023-03-17 00:04:53 +0900

    Connect database successfully

--- app/core/models/store.py
+++ app/core/models/store.py
@@ -1,8 +1,21 @@
 from pydantic import BaseModel, Field
+from bson.objectid import ObjectId
+from enum import Enum
+
+class Status(Enum):
+    open = 1
+    close = 2
+
 
 class StoreModel(BaseModel):
+    _id: ObjectId | None
     name: str = Field(title="name of restuarant")
-    m_id: str = Field(title="menu identifier currently in use by restaurant")
+    desc: str = Field(title="description of the restuarant")
+    schedule: str = Field(title="schedule of the restuarant")
+    notice: str | None = Field(title="notice of the restuarant")
+    status: Status = Field(title="Restaurant operation status[open:1/close:2]")
+    img_src: str = Field(title="URI related to image request")
+    m_id: str | None = Field(title="menu identifier currently in use by restaurant")
     
 class FoodModel(BaseModel):
     name: str = Field(title="name of food")
