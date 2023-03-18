8dbce33e1e0ee3f71a0171c9388c0d585c352673
seo <deergirl11@gmail.com>
2023-03-18 15:00:28 +0900

    replace components & keep backgroundColor when button is clicked

--- src/pages/StoreManagePage.js
+++ src/pages/StoreManagePage.js
@@ -1,8 +1,7 @@
 import MenuBar from "../components/MenuBar";
 import Store from "../css/StoreManage.module.css";
 import Button from "../css/Button.module.css";
-import { useState } from "react";
-import { useMediaQuery } from "react-responsive";
+import { useState, useEffect } from "react";
 
 function StoreManagePage() {
   const [desc, setDesc] = useState("우리 가게는요 수제 버거 전문점으로 ~");
@@ -12,11 +11,6 @@ function StoreManagePage() {
   );
   const [editDescAndSchedule, setEditDescAndSchedule] = useState(false);
   const [editNoti, setEditNoti] = useState(false);
-  const [activeBtn, setActiveBtn] = useState(null);
-
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
-  const isDesktop = useMediaQuery({ minWidth: 1024 });
 
   const handleEditDescAndScheduleClick = () => {
     setEditDescAndSchedule(true);
@@ -31,89 +25,107 @@ function StoreManagePage() {
     setEditNoti(false);
   };
 
-  const handleButtonClick = (btnId) => {
-    setActiveBtn(btnId);
+  /*상태바 변경*/
+  const [currentClick, setCurrentClick] = useState(null);
+  const [prevClick, setPrevClick] = useState(null);
+
+  const onClickBtn = (e) => {
+    setCurrentClick(e.target.id);
   };
 
-  /*   가게 상태 버튼의 상태에 따라 통신
-  const renderContent = () => {
-    if (activeBtn === 1) {
+  useEffect(
+    (e) => {
+      if (currentClick !== null) {
+        let current = document.getElementById(currentClick);
+        current.style.backgroundColor = "#ff9345";
+      }
 
-    }else if (activeBtn === 2) {
-
-    }
-  } */
+      if (prevClick !== null) {
+        let prev = document.getElementById(prevClick);
+        prev.style.backgroundColor = "#ffffff";
+      }
+      setPrevClick(currentClick);
+    },
+    [currentClick]
+  );
 
   return (
     <div>
       <section className="header">
         <MenuBar />
       </section>
-      <div className={Store.inner}>
-        <section className="status-bar">
+      <div className={Store.content}>
+        <div className={Store.statusBar}>
           <div className={Store.status}>
             <button
               className={Store.statusButton}
-              activeColor="#ff9345"
-              onClick={() => handleButtonClick(1)}
+              id="open"
+              onClick={onClickBtn}
             >
               영업 중
             </button>
             <button
               className={Store.statusButton}
-              activeColor="#ff9345"
-              onClick={() => handleButtonClick(2)}
+              id="close"
+              onClick={onClickBtn}
             >
               영업 종료
             </button>
           </div>
-        </section>
-        <div className={Store.content}>
-          <section className="description">
-            <div className={Store.desc}>
-              <h1>000 가게</h1>
-              <div className={Store.info}>
-                <h3>가게 한줄 소개</h3>
-                {editDescAndSchedule ? (
-                  <input
-                    type="text"
-                    value={desc}
-                    onChange={(e) => setDesc(e.target.value)}
-                  />
-                ) : (
-                  <p>{desc}</p>
-                )}
-                <h3>운영시간</h3>
-                {editDescAndSchedule ? (
-                  <input
-                    type="text"
-                    value={schedule}
-                    onChange={(e) => setSchedule(e.target.value)}
-                  />
-                ) : (
-                  <p>{schedule}</p>
-                )}
+        </div>
 
-                {editDescAndSchedule ? (
-                  //show edit button when editing
-                  <button className={Button.modify} onClick={handleSaveClick}>
-                    저장
-                  </button>
-                ) : (
-                  //show modify button when not editing
-                  <button
-                    className={Button.modify}
-                    onClick={handleEditDescAndScheduleClick}
-                  >
-                    수정
-                  </button>
-                )}
-              </div>
+        <section className={Store.description}>
+          <h1>000 가게</h1>
+          <div className={Store.desc}>
+            <div className={Store.intro}>
+              <h3>가게 한줄 소개</h3>
+              {editDescAndSchedule ? (
+                <input
+                  type="text"
+                  value={desc}
+                  onChange={(e) => setDesc(e.target.value)}
+                />
+              ) : (
+                <p>{desc}</p>
+              )}
             </div>
-          </section>
-          <section className="notice">
-            <div className={Store.notice}>
-              <h1>가게 공지사항</h1>
+            <div className={Store.opentime}>
+              <h3>운영시간</h3>
+              {editDescAndSchedule ? (
+                <input
+                  type="text"
+                  value={schedule}
+                  onChange={(e) => setSchedule(e.target.value)}
+                />
+              ) : (
+                <p>{schedule}</p>
+              )}
+            </div>
+
+            {editDescAndSchedule ? (
+              //show edit button when editing
+              <button
+                className={`${Button.modify} ${Store.modifyBtn}`}
+                onClick={handleSaveClick}
+              >
+                저장
+              </button>
+            ) : (
+              //show modify button when not editing
+              <button
+                className={`${Button.modify} ${Store.modifyBtn}`}
+                onClick={handleEditDescAndScheduleClick}
+              >
+                수정
+              </button>
+            )}
+          </div>
+        </section>
+
+        <section className={Store.noticeBlock}>
+          <h1>가게 공지사항</h1>
+          <div className={Store.notice}>
+            <div className={Store.innerNotice}>
               {editNoti ? (
                 <input
                   type="text"
@@ -123,25 +135,26 @@ function StoreManagePage() {
               ) : (
                 <p>{noti}</p>
               )}
-              <div className={Store.buttons}>
-                {editNoti ? (
-                  //show edit button when editing
-                  <button className={Button.modify} onClick={handleSaveClick}>
-                    저장
-                  </button>
-                ) : (
-                  //show modify button when not editing
-                  <button
-                    className={Button.modify}
-                    onClick={handleEditNotiClick}
-                  >
-                    수정
-                  </button>
-                )}
-              </div>
             </div>
-          </section>
-        </div>
+            {editNoti ? (
+              //show edit button when editing
+              <button
+                className={`${Button.modify} ${Store.modifyBtn}`}
+                onClick={handleSaveClick}
+              >
+                저장
+              </button>
+            ) : (
+              //show modify button when not editing
+              <button
+                className={`${Button.modify} ${Store.modifyBtn}`}
+                onClick={handleEditNotiClick}
+              >
+                수정
+              </button>
+            )}
+          </div>
+        </section>
       </div>
     </div>
   );
