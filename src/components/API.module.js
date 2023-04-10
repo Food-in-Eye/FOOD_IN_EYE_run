a02781bbbb3d84a4baa7f3ecb0a8a9867e9377ca
seo <deergirl11@gmail.com>
2023-04-10 16:12:41 +0900

    add codes GET Menu image from API & update Menu Infos

--- src/components/API.module.js
+++ src/components/API.module.js
@@ -2,13 +2,32 @@ import axios from "axios";
 
 const STORE_URL = "/api/v1/admin/stores";
 const MENUS_URL = "/api/v1/admin/foods";
+const FOOD_IMG_URL = MENUS_URL + "/image";
 
+/** GET 메서드 */
 //가게 ID에 따라 URL을 동적으로 구성하는 getStore() 함수
 export const getStore = (storeId) => {
   const requestUrl = `${STORE_URL}/${storeId}`;
   return axios.get(requestUrl);
 };
 
+//메뉴 ID에 따라 URL을 동적으로 구성하는 getMenus() 함수
+export const getMenus = (s_id) => {
+  const requestUrl = `${MENUS_URL}/?s_id=${s_id}`;
+  return axios.get(requestUrl);
+};
+
+export const getMenu = (m_id) => {
+  const requestUrl = `${MENUS_URL}/${m_id}`;
+  return axios.get(requestUrl);
+};
+
+export const getFoodImg = (f_id) => {
+  const requestUrl = `${FOOD_IMG_URL}/?f_id=${f_id}`;
+  return axios.get(requestUrl);
+};
+
+/** PUT 메서드 */
 //가게 ID에 따라 URL을 동적으로 구성하는 putStore() 함수
 export const putStore = (storeId, data) => {
   const requestUrl = `${STORE_URL}/${storeId}`;
@@ -21,16 +40,9 @@ export const putStore = (storeId, data) => {
   });
 };
 
-/** ID 변경사항 적용할 예정(0407) */
-//메뉴 ID에 따라 URL을 동적으로 구성하는 getMenus() 함수
-export const getMenus = (menuId) => {
-  const requestUrl = `${MENUS_URL}/${menuId}`;
-  return axios.get(requestUrl);
-};
-
 //메뉴 ID에 따라 URL을 동적으로 구성하는 putMenus() 함수
-export const putMenus = (menuId, data) => {
-  const requestUrl = `${MENUS_URL}/${menuId}`;
+export const putMenus = (s_id, data) => {
+  const requestUrl = `${MENUS_URL}/${s_id}`;
 
   return axios.put(requestUrl, JSON.stringify(data), {
     headers: {
--- src/pages/MenuManagePage.js
+++ src/pages/MenuManagePage.js
@@ -4,69 +4,40 @@ import Menu from "../css/MenuManage.module.css";
 import Button from "../css/Button.module.css";
 import Bar from "../css/UnderBar.module.css";
 
-import axios from "axios";
-import { getMenus, putMenus } from "../components/API.module";
-import Dropzone from "react-dropzone";
-import "swiper/swiper-bundle.css";
+import {
+  getFoodImg,
+  getMenu,
+  getMenus,
+  putMenus,
+} from "../components/API.module";
+// import "swiper/swiper-bundle.css";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 
 function MenuManagePage() {
-  /** add menus */
-  const [menus, setMenus] = useState([
-    "메뉴1",
-    "메뉴2",
-    "메뉴3",
-    "메뉴4",
-    "메뉴5",
-  ]);
-  const [showButtons, setShowButtons] = useState(
-    Array(menus.length).fill(false)
-  );
-
-  const handleAddMenu = () => {
-    const newMenu = `메뉴${menus.length + 1}`;
-    setMenus([...menus, newMenu]);
-  };
-
-  const handleDeleteMenu = (index) => {
-    const newMenus = [...menus];
-    newMenus.splice(index, 1);
-    setMenus(newMenus);
-
-    const newButtons = [...showButtons];
-    newButtons.splice(index, 1);
-    setShowButtons(newButtons);
-  };
-
-  const toggleButton = (index) => {
-    const newButtons = [...showButtons];
-    newButtons[index] = !newButtons[index];
-    setShowButtons(newButtons);
-  };
-
-  /**통신 부분 -> 모듈화 진행할 예정 */
-  const [menu, setMenu] = useState({});
+  /** api에서 불러올 menuList */
+  const [menuList, setMenuList] = useState([]);
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(null);
 
-  /**사용자 입력값을 저장하는 변수 */
-  const [editValue, setEditValue] = useState("");
   const [editMenu, setEditMenu] = useState(false);
 
-  //GET 메서드
+  const [selectedMenuId, setSelectedMenuId] = useState([]);
+  const [selectedMenu, setSelectedMenu] = useState([]);
+  const [selectedMenuImgURL, setSelectedMenuImgURL] = useState(``);
+
+  /** GET 메서드 */
   useEffect(() => {
     //GET 요청을 보내서 데이터 반영
     const fetchMenu = async () => {
       try {
         /**요청 시작 시 error과 menu 초기화*/
         setError(null);
-        setMenu([]);
         //loading 상태는 true로 세팅
         setLoading(true);
 
-        const request = await getMenus("642506e3cc80984129fe0209");
-        setMenu(request.data.response);
+        const request = await getMenus("641459134443f2168a32357b");
+        setMenuList(request.data.response);
       } catch (e) {
         setError(e);
       }
@@ -76,32 +47,86 @@ function MenuManagePage() {
     fetchMenu();
   }, []);
 
+  /**리렌더링 최적화 */
+  const memorizedGetMenu = useMemo(
+    () => getMenu(selectedMenuId),
+    [selectedMenuId]
+  );
+
+  /** 선택한 menu -> menuId에 따라 get */
+  async function handleMenuClick(e, menuId) {
+    e.preventDefault();
+
+    setSelectedMenuId(menuId);
+    try {
+      const requestMenu = await memorizedGetMenu;
+      setSelectedMenu(requestMenu.data.response);
+      setSelectedMenuImgURL(
+        `https://foodineye.s3.ap-northeast-2.amazonaws.com/${requestMenu.data.response.img_key}`
+      );
+    } catch (e) {
+      setError(e);
+    }
+  }
+
+  /** 메뉴 삭제 관련 코드 */
+  /** ----------------------------------------------------- */
+  // const handleAddMenu = () => {
+  //   const newMenu = ` 메뉴 `;
+  //   setMenus([...menuList, newMenu]);
+  // };
+
+  // const [showButtons, setShowButtons] = useState(
+  //   Array(menuList.length).fill(false)
+  // );
+
+  // const handleDeleteMenu = (index) => {
+  //   const newMenus = [...menus];
+  //   newMenus.splice(index, 1);
+  //   setMenus(newMenus);
+
+  //   const newButtons = [...showButtons];
+  //   newButtons.splice(index, 1);
+  //   setShowButtons(newButtons);
+  // };
+
+  // const toggleButton = (index) => {
+  //   const newButtons = [...showButtons];
+  //   newButtons[index] = !newButtons[index];
+  //   setShowButtons(newButtons);
+  // };
+  /** ----------------------------------------------------- */
+
   /** edit descriptions of menu when click modify button */
-  const [menuName, setMenuName] = useState(menu.name);
-  const [menuDesc, setMenuDesc] = useState(menu.desc);
-  const [menuAllergy, setMenuAllergy] = useState(menu.allergy);
-  const [originItems, setOriginItems] = useState(menu.origin);
-  const [menuPrice, setMenuPrice] = useState(menu.price);
+  const [menuName, setMenuName] = useState(selectedMenu.name);
+  const [menuDesc, setMenuDesc] = useState(selectedMenu.desc);
+  const [menuAllergy, setMenuAllergy] = useState(selectedMenu.allergy);
+  const [originItems, setOriginItems] = useState(selectedMenu.origin);
+  const [menuPrice, setMenuPrice] = useState(selectedMenu.price);
 
   /** edit menuImage */
-  const [menuImg, setMenuImg] = useState(null);
-  const [menuImgMsg, setMenuImgMsg] = useState("메뉴 이미지 수정하기");
-  const formData = new FormData(); //추가한 메뉴 이미지 저장
+  const [menuImg, setMenuImg] = useState(selectedMenuImgURL);
+  const imgRef = useRef(null);
 
   /** 메뉴 세부내용 수정 */
   const handleEditMenuClick = (e, index) => {
-    setMenuName(menu.name);
-    setMenuDesc(menu.desc);
-    setMenuAllergy(menu.allergy);
-    setOriginItems(menu.origin);
-    setMenuPrice(menu.price);
+    e.preventDefault();
+
+    setMenuName(selectedMenu.name);
+    setMenuImg(selectedMenuImgURL);
+    setMenuDesc(selectedMenu.desc);
+    setMenuAllergy(selectedMenu.allergy);
+    setOriginItems(selectedMenu.origin);
+    setMenuPrice(selectedMenu.price);
     setEditMenu(true);
   };
 
   /** 수정 내용 저장 */
-  const handleSaveMenuClick = () => {
-    putMenus("642506e3cc80984129fe0209", {
-      ...menu,
+  const handleSaveMenuClick = (e) => {
+    e.preventDefault();
+
+    putMenus("641d952818f0b258e9ca025b", {
+      ...selectedMenu,
       name: menuName,
       price: menuPrice,
       desc: menuDesc,
@@ -109,15 +134,30 @@ function MenuManagePage() {
       origin: originItems,
     })
       .then((res) => {
-        setMenu((prevState) => ({
-          ...prevState,
+        setSelectedMenu({
+          ...selectedMenu,
           name: menuName,
           price: menuPrice,
           desc: menuDesc,
           allergy: menuAllergy,
           origin: originItems,
-        }));
+        });
         setEditMenu(false);
+
+        //menuList 상태 업데이트
+        const menuIndex = menuList.findIndex(
+          (menu) => menu._id === selectedMenuId
+        );
+        const updatedMenuList = [...menuList];
+        updatedMenuList[menuIndex] = {
+          ...selectedMenu,
+          name: menuName,
+          price: menuPrice,
+          desc: menuDesc,
+          allergy: menuAllergy,
+          origin: originItems,
+        };
+        setMenuList(updatedMenuList);
       })
       .catch((e) => {
         console.log(e.response);
@@ -129,148 +169,175 @@ function MenuManagePage() {
       <section className="header">
         <MenuBar />
       </section>
-      <div className={Menu.inner}>
-        <section className="menus">
-          <div className={Menu.menus}>
-            <h1>Menu</h1>
-            <div className={Bar.line}>
-              <div className={Bar.circle}></div>
+      {menuList && (
+        <div className={Menu.inner}>
+          <section className="menus">
+            <div className={Menu.menus}>
+              <h1>Menu</h1>
+              <div className={Bar.line}>
+                <div className={Bar.circle}></div>
+              </div>
+              <ul>
+                {menuList.map((menu) => (
+                  <li
+                    key={menu._id}
+                    data-hammer
+                    className={Menu.list}
+                    onClick={(e) => handleMenuClick(e, menu._id)}
+                  >
+                    <span>{menu.name}</span>
+                    {/* {showButtons[index] && (
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleDeleteMenu(index);
+                        }}
+                      >
+                        삭제
+                      </button> */}
+                  </li>
+                ))}
+              </ul>
+              <button className={Button.plus}>+</button>
             </div>
-            <ul>
-              {menus.map((menu, index) => (
-                <li
-                  key={index}
-                  data-hammer
-                  className={Menu.list}
-                  onClick={() => toggleButton(index)}
-                >
-                  <span>{menu}</span>
-                  {showButtons[index] && (
+          </section>
+          <hr className={Menu.vertical} />
+          {selectedMenu.length !== 0 && (
+            <section className="menuInfo">
+              <div className={Menu.info}>
+                {editMenu ? (
+                  <input
+                    className={Menu.menuName}
+                    type="text"
+                    value={menuName}
+                    cols="true"
+                    onChange={(e) => setMenuName(e.target.value)}
+                  />
+                ) : (
+                  <h2>{selectedMenu.name}</h2>
+                )}
+                <div className={Bar.line}>
+                  <div className={Bar.circle}></div>
+                </div>
+                <div className={Menu.Img}>
+                  {editMenu ? (
+                    <form className={Menu.editMenus}>
+                      <img
+                        className={Menu.menuImg}
+                        src={selectedMenuImgURL}
+                        alt="메뉴 이미지"
+                      />
+                      <label className={Menu.foodImgLabel} htmlFor="foodImg">
+                        메뉴 이미지 추가
+                      </label>
+                      <br />
+                      <input
+                        className={Menu.foodImgInput}
+                        type="file"
+                        accept="image/*"
+                        id="foodImg"
+                        // onChange={saveImgFile}
+                        ref={imgRef}
+                      />
+                    </form>
+                  ) : selectedMenuImgURL !== "" ? (
+                    <img
+                      className={Menu.menuImg}
+                      src={selectedMenuImgURL}
+                      alt="메뉴 이미지"
+                    />
+                  ) : (
+                    <img
+                      className={Menu.menuImg}
+                      src={""}
+                      alt="메뉴 이미지 없음"
+                    />
+                  )}
+                </div>
+                <br />
+                <div className={Menu.details}>
+                  <div className={Menu.description}>
+                    <h3>메뉴 설명</h3>
+                    {editMenu ? (
+                      <textarea
+                        type="text"
+                        value={menuDesc}
+                        cols="true"
+                        rows="3"
+                        onChange={(e) => setMenuDesc(e.target.value)}
+                      />
+                    ) : (
+                      <p>{selectedMenu.desc}</p>
+                    )}
+                  </div>
+                  <div className={Menu.allergy}>
+                    <h3>알레르기</h3>
+                    {editMenu ? (
+                      <textarea
+                        type="text"
+                        value={menuAllergy}
+                        cols="true"
+                        rows="2"
+                        onChange={(e) => setMenuAllergy(e.target.value)}
+                      />
+                    ) : (
+                      <p>{selectedMenu.allergy}</p>
+                    )}
+                  </div>
+                  <div className={Menu.origin}>
+                    <h3>원산지 표기</h3>
+                    {editMenu ? (
+                      <textarea
+                        type="text"
+                        value={originItems}
+                        cols="true"
+                        rows="3"
+                        onChange={(e) => setOriginItems(e.target.value)}
+                      />
+                    ) : (
+                      <p>{selectedMenu.origin}</p>
+                    )}
+                  </div>
+                  <div className={Menu.price}>
+                    <h3>가격</h3>
+                    {editMenu ? (
+                      <textarea
+                        type="text"
+                        value={menuPrice}
+                        cols="true"
+                        rows="1"
+                        onChange={(e) => setMenuPrice(e.target.value)}
+                      />
+                    ) : (
+                      <p>{selectedMenu.price}</p>
+                    )}
+                  </div>
+                </div>
+                <div className={Menu.buttons}>
+                  {editMenu ? (
                     <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        handleDeleteMenu(index);
-                      }}
+                      className={Button.saveMenu}
+                      onClick={(e) => handleSaveMenuClick(e)}
                     >
-                      삭제
+                      저장하기
+                    </button>
+                  ) : (
+                    <button
+                      className={Button.modifyMenu}
+                      onClick={(e) => handleEditMenuClick(e)}
+                    >
+                      수정하기
                     </button>
                   )}
-                </li>
-              ))}
-            </ul>
-            <button className={Button.plus} onClick={handleAddMenu}>
-              +
-            </button>
-          </div>
-        </section>
-        <hr className={Menu.vertical} />
-        <section className="menuInfo">
-          <div className={Menu.info}>
-            {editMenu ? (
-              <input
-                className={Menu.menuName}
-                type="text"
-                value={menuName}
-                cols="true"
-                onChange={(e) => setMenuName(e.target.value)}
-              />
-            ) : (
-              <h2>{menu.name}</h2>
-            )}
-            <div className={Bar.line}>
-              <div className={Bar.circle}></div>
-            </div>
-            <div className={Menu.Img}>
-              <img
-                className={Menu.menuImg}
-                src={require("../images/tizle.jpeg")}
-                alt="메뉴: 티즐"
-              />
-              <img
-                className={Menu.editBtn}
-                src={require("../images/edit.jpeg")}
-                alt="메뉴 이미지 수정하기"
-              />
-            </div>
-            <br />
-            <div className={Menu.details}>
-              <div className={Menu.description}>
-                <h3>메뉴 설명</h3>
-                {editMenu ? (
-                  <textarea
-                    type="text"
-                    value={menuDesc}
-                    cols="true"
-                    rows="3"
-                    onChange={(e) => setMenuDesc(e.target.value)}
-                  />
-                ) : (
-                  <p>{menu.desc}</p>
-                )}
+                </div>
               </div>
-              <div className={Menu.allergy}>
-                <h3>알레르기</h3>
-                {editMenu ? (
-                  <textarea
-                    type="text"
-                    value={menuAllergy}
-                    cols="true"
-                    rows="2"
-                    onChange={(e) => setMenuAllergy(e.target.value)}
-                  />
-                ) : (
-                  <p>{menu.allergy}</p>
-                )}
-              </div>
-              <div className={Menu.origin}>
-                <h3>원산지 표기</h3>
-                {editMenu ? (
-                  <textarea
-                    type="text"
-                    value={originItems}
-                    cols="true"
-                    rows="3"
-                    onChange={(e) => setOriginItems(e.target.value)}
-                  />
-                ) : (
-                  <p>{menu.origin}</p>
-                )}
-              </div>
-              <div className={Menu.price}>
-                <h3>가격</h3>
-                {editMenu ? (
-                  <textarea
-                    type="text"
-                    value={menuPrice}
-                    cols="true"
-                    rows="1"
-                    onChange={(e) => setMenuPrice(e.target.value)}
-                  />
-                ) : (
-                  <p>{menu.price}</p>
-                )}
-              </div>
-            </div>
-            <div className={Menu.buttons}>
-              {editMenu ? (
-                <button
-                  className={Button.saveMenu}
-                  onClick={handleSaveMenuClick}
-                >
-                  저장하기
-                </button>
-              ) : (
-                <button
-                  className={Button.modifyMenu}
-                  onClick={handleEditMenuClick}
-                >
-                  수정하기
-                </button>
-              )}
-            </div>
-          </div>
-        </section>
-      </div>
+            </section>
+          )}
+          {selectedMenu.length === 0 && (
+            <h2 className={Menu.noMenuClicked}>메뉴를 선택해주세요.</h2>
+          )}
+        </div>
+      )}
     </div>
   );
 }
