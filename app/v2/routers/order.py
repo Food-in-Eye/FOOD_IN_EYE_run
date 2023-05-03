19d4f4b55e2fa4d91e9a0d20d835c67748683f17
JIIN <j00j12i11@duksung.ac.kr>
2023-05-04 03:44:46 +0900

    dd

--- app/v2/routers/order.py
+++ app/v2/routers/order.py
@@ -18,8 +18,29 @@ DB = MongodbController('FIE_DB')
 async def hello():
     return {"message": f"Hello '{PREFIX}'"}
 
+@order_router.get("/")
+async def get_order(s_id: str, today=False):
+    ''' 특정 가게의 주문 내역을 여러개 찾아서 반환한다. 
+        "today" -> True일 경우 오늘 주문 내역만 반환한다. (기본값은 False)
+        "asce" -> True일 경우 데이터를 오름차순으로 정렬하여 반환한다. (기본값은 False)
+    '''
+    pass
+
+@order_router.get("/order")
+async def get_order(id: str):
+    ''' 특정 id에 대한 주문내역을 찾아서 반환'''
+    pass
+
 @order_router.post("/order")
 async def new_order(body:OrderModel):
+    ''' app으로 부터 주문을 받고 처리한다.
+        [check 할 사항들]
+        1. 사용자로부터 주문을 받아 가게별 주문으로 나누어 order DB에 저장한다.
+        2. 주문에 해당하는 가게에 websocket을 통해 메시지를 보낸다.
+        3. 주문에 대한 데이터를 history DB에 추가한다. 
+            -> (주문자 정보, 주문 날짜, 주문 총 가격, 시선데이터 경로, 주문 식별자 리스트)
+        4. 모든 과정이 마무리되면 OK 응답을 보낸다.
+    '''
     try:
         order_id_list = []
         for store_order in body.content:
