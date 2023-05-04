05338dce621aae1587162efe7e93341e12735f9b
JIIN <j00j12i11@duksung.ac.kr>
2023-05-05 08:03:35 +0900

    add order_router's functions

--- app/v2/routers/order.py
+++ app/v2/routers/order.py
@@ -7,7 +7,7 @@ from core.models import OrderModel
 from core.common.mongo2 import MongodbController
 from .src.util import Util
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 order_router = APIRouter(prefix="/orders")
 
@@ -19,32 +19,119 @@ async def hello():
     return {"message": f"Hello '{PREFIX}'"}
 
 @order_router.get("/")
-async def get_order(s_id: str, today=False):
-    ''' 특정 가게의 주문 내역을 여러개 찾아서 반환한다. 
-        "today" -> True일 경우 오늘 주문 내역만 반환한다. (기본값은 False)
-        "asce" -> True일 경우 데이터를 오름차순으로 정렬하여 반환한다. (기본값은 False)
+async def get_order(s_id: str=None, u_id: str=None, today: bool=False, ascending: bool=True):
+    ''' 특정 조건을 만족하는 주문 내역 전체를 반환한다.
+        - s_id: 주문을 받은 가게가 일치하는 주문내역만 가져온다.
+        - u_id: 주문자가 일치하는 주문내역만 가져온다.
+        - today: True인 경우, 요청 날짜의 주문내역만 가져온다.
+        - ascending: True인 경우, 오름차순으로 정렬한다.
     '''
-    pass
+    query = {}
+    q_str_list = []
+    if s_id:
+        query["s_id"] = s_id
+        q_str_list.append(f"s_id={s_id}")
+    if u_id:
+        query["u_id"] = u_id
+        q_str_list.append(f"u_id={u_id}")
+    if today:
+        today_str = datetime.today().strftime("%Y-%m-%d")
+        today_start = datetime.strptime(today_str, "%Y-%m-%d")
+        today_end = today_start + timedelta(days=1)
+        query["date"] = {"$gte": today_start, "$lt": today_end}
+        q_str_list.append(f"today={today}")
+    
+    if ascending:
+        q_str_list.append(f"ascending={ascending}")
+    
+    if len(q_str_list) > 1:
+        q_str = q_str_list.pop(0)
+        for str in q_str_list:
+            q_str = q_str + "&" + str
+
+    try:
+        response = DB.read_all_by_query('order', query, ascending)
+    
+    except Exception as e:
+        print('ERROR', e)
+        return {
+            'request': f'GET {PREFIX}?{q_str}',
+            'status': 'ERROR',
+            'message': f'ERROR {e}'
+        }
+    
+    return {
+        'request': f'GET {PREFIX}?{q_str}',
+        'status': 'OK',
+        'response': response
+    }
 
 @order_router.get("/order")
 async def get_order(id: str):
     ''' 특정 id에 대한 주문내역을 찾아서 반환'''
-    pass
+
+    try:
+        Util.check_id(id)
+
+        response = DB.read_by_id('order', id)
+    except Exception as e:
+        print('ERROR', e)
+        return {
+            'request': f'GET {PREFIX}/order?id={id}',
+            'status': 'ERROR',
+            'message': f'ERROR {e}'
+        }
+    
+    return {
+        'request': f'GET {PREFIX}/order?id={id}',
+        'status': 'OK',
+        'response': response
+    }
+
+@order_router.put("/order/status")
+async def change_status(id: str):
+    ''' 특정 주문 내역의 진행 상태를 변경한다.'''
+    
+    try:
+        Util.check_id(id)
+        response = DB.read_by_id('order', id)
+        s = response['status']
+        
+        if s < 2:
+            DB.update_field_by_id('order', id, 'status', s+1)
+        else:
+            raise Exception('status is already finish')
+
+    except Exception as e:
+        print('ERROR', e)
+        return {
+            'request': f'GET {PREFIX}/order?id={id}',
+            'status': 'ERROR',
+            'message': f'ERROR {e}'
+        }
+    
+    return {
+        'request': f'GET {PREFIX}/order?id={id}',
+        'status': 'OK'
+    }
 
 @order_router.post("/order")
 async def new_order(body:OrderModel):
-    ''' app으로 부터 주문을 받고 처리한다.
+    ''' app으로 부터 주문을 받고 처리한다.        
         [check 할 사항들]
-        1. 사용자로부터 주문을 받아 가게별 주문으로 나누어 order DB에 저장한다.
+        1. 사용자로부터 주문을 받아 가게별 주문으로 나누어 order DB에 저장한다. (O)
         2. 주문에 해당하는 가게에 websocket을 통해 메시지를 보낸다.
         3. 주문에 대한 데이터를 history DB에 추가한다. 
             -> (주문자 정보, 주문 날짜, 주문 총 가격, 시선데이터 경로, 주문 식별자 리스트)
-        4. 모든 과정이 마무리되면 OK 응답을 보낸다.
+        
+        모든 과정이 마무리되면 OK 응답을 보낸다.
     '''
     try:
         order_id_list = []
         for store_order in body.content:
+            print(store_order)
             order = {
+                "u_id": body.u_id,
                 "s_id": store_order.s_id,
                 "m_id": store_order.m_id,
                 "f_list": store_order.f_list
@@ -60,10 +147,10 @@ async def new_order(body:OrderModel):
            "orders": order_id_list
         }
         
-        h_id = str(DB.create('test', history))
+        h_id = str(DB.create('test', history)) # todo: colection 수정 필요
 
-        print("h_id:", h_id)
-        print("\t", history)
+        # print("h_id:", h_id)
+        # print("\t", history)
         return {
             'request': f'POST {PREFIX}/order',
             'status': 'OK'
