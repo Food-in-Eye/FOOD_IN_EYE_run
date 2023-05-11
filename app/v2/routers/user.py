6fcbcad6f95002cd58b9d38e57e6124845fb16a9
mi642 <68195384+mi642@users.noreply.github.com>
2023-05-12 01:04:46 +0900

    modify users - del token

--- app/v2/routers/user.py
+++ app/v2/routers/user.py
@@ -52,13 +52,11 @@ html = """
             loginForm.addEventListener('submit', async (e) => {
                 e.preventDefault();
                 const username = document.getElementById('username').value;
-                const response = await fetch(`http://127.0.0.1:8000/api/v2/users/login?id=${username}`);
-                const result = await response.json();
-                const token = result.response;
-                if (token) {
+
+                if (1) {
                     const wsId = username;
                     wsIdElem.textContent = wsId;
-                    socket = new WebSocket(`${wsUrl}?id=${wsId}&token=${token}`);
+                    socket = new WebSocket(`${wsUrl}?id=${wsId}`);
                     socket.onmessage = function(event) {
                         const messages = document.getElementById('messages');
                         const message = document.createElement('li');
@@ -212,26 +210,22 @@ async def client_get():
 #     return f'hi {manager.active_connections}'
 
 # websocket을 연견할 때 서버가 id, token이 일치하는지 확인하는 코드
-async def check_token(id : str, token : str):
-    user = DB.read_by_id('user', id)
+# async def check_token(id : str, token : str):
+#     user = DB.read_by_id('user', id)
 
-    if str(user['token']) == token:
-        return user['_id']
-    else :
-        return False
+#     if str(user['token']) == token:
+#         return user['_id']
+#     else :
+#         return False
 
 @user_router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket, id : str, token : str): # token 이 추가되어야 함
+async def websocket_endpoint(websocket: WebSocket, id : str): # token 이 추가되어야 함
     try:
         await manager.connect(id, websocket)
-        if await check_token(id, token):
-            # asyncio.ensure_future(manager.send_heartbeat(websocket))
 
-            while True:
-                data = await manager.receive_json(id) # websocket 형식 정하고 json으로 바꿀 것 json_str = json.dumps(data)
-                await manager.handle_message(id, data)
-        else:
-            raise WebSocketDisconnect(f'The client({websocket.client})\'s token does not match.')
+        while True:
+            data = await manager.receive_json(id) # websocket 형식 정하고 json으로 바꿀 것 json_str = json.dumps(data)
+            await manager.handle_message(id, data)
 
     except WebSocketDisconnect as d:
         print(f'Websocket ERROR : {d}')
@@ -242,102 +236,102 @@ async def websocket_endpoint(websocket: WebSocket, id : str, token : str): # tok
 
 
 
-import random
+# import random
 
-def generate_token():
-    return random.randint(10000, 99999)
+# def generate_token():
+#     return random.randint(10000, 99999)
 
 @user_router.get("/hello")
 async def hello():
     return {"message": f"Hello '{PREFIX}'"}
 
-@user_router.get("/login")
-async def client_check_login(id : str): # 로그인으로 확장
-    try:
-        Util.check_id(id)
+# @user_router.get("/login")
+# async def client_check_login(id : str): # 로그인으로 확장
+#     try:
+#         Util.check_id(id)
 
-        user = DB.read_by_id('user', id)
+#         user = DB.read_by_id('user', id)
 
-        if user:
-            token = generate_token()
-            del user['_id'] # 삭제 하지 않는 경우 오류 발생 _id는 수정 불가
-            user['token'] = token
-            DB.update_by_id('user', id, user)
+#         if user:
+#             token = generate_token()
+#             del user['_id'] # 삭제 하지 않는 경우 오류 발생 _id는 수정 불가
+#             user['token'] = token
+#             DB.update_by_id('user', id, user)
 
-            response = token
+#             response = token
         
-    except Exception as e:
-        print('ERROR', e)
-        return {
-            'request': f'{PREFIX}/login?id={id}',
-            'status': 'ERROR',
-            'message': f'ERROR {e}'
-        }
+#     except Exception as e:
+#         print('ERROR', e)
+#         return {
+#             'request': f'{PREFIX}/login?id={id}',
+#             'status': 'ERROR',
+#             'message': f'ERROR {e}'
+#         }
     
-    return {
-        'request': f'{PREFIX}/login?id={id}',
-        'status': 'OK',
-        'response': response
-    }
+#     return {
+#         'request': f'{PREFIX}/login?id={id}',
+#         'status': 'OK',
+#         'response': response
+#     }
 
-# check_token랑 코드 일치한데 애매함
-@user_router.get("/token")
-async def client_check_token(id : str, token : str):
-    try:
-        user = DB.read_by_id('user', id)
+# # check_token랑 코드 일치한데 애매함
+# @user_router.get("/token")
+# async def client_check_token(id : str, token : str):
+#     try:
+#         user = DB.read_by_id('user', id)
 
-        if str(user['token']) == token:
-            response =  user['_id']
+#         if str(user['token']) == token:
+#             response =  user['_id']
 
-    except Exception as e:
-        print('ERROR', e)
-        return {
-            'request': f'{PREFIX}/token?id={id}?token={token}',
-            'status': 'ERROR',
-            'message': f'ERROR {e}'
-        }
+#     except Exception as e:
+#         print('ERROR', e)
+#         return {
+#             'request': f'{PREFIX}/token?id={id}?token={token}',
+#             'status': 'ERROR',
+#             'message': f'ERROR {e}'
+#         }
     
-    return {
-        'request': f'{PREFIX}/token?id={id}?token={token}',
-        'status': 'OK',
-        'response': response
-    }
+#     return {
+#         'request': f'{PREFIX}/token?id={id}?token={token}',
+#         'status': 'OK',
+#         'response': response
+#     }
 
 # token 삭제 -> http 뺄지 말지 고민
-@user_router.get("/logout")
-async def del_token(id : str): # 토큰 삭제
-    try:
-        user = DB.read_by_id('user', id)
+# @user_router.get("/logout")
+# async def del_token(id : str): # 토큰 삭제
+#     try:
+#         user = DB.read_by_id('user', id)
 
-        if user:
-            del user['_id']
-            user['token'] = ''
-            DB.update_by_id('user', id, user)
+#         if user:
+#             del user['_id']
+#             user['token'] = ''
+#             DB.update_by_id('user', id, user)
 
-    except Exception as e:
-        print('ERROR', e)
-        return {
-            'request': f'{PREFIX}/logout?id={id}',
-            'status': 'ERROR',
-            'message': f'ERROR {e}'
-        }
+#     except Exception as e:
+#         print('ERROR', e)
+#         return {
+#             'request': f'{PREFIX}/logout?id={id}',
+#             'status': 'ERROR',
+#             'message': f'ERROR {e}'
+#         }
     
-    return {
-        'request': f'{PREFIX}/logout?id={id}',
-        'status': 'OK',
-    }
+#     return {
+#         'request': f'{PREFIX}/logout?id={id}',
+#         'status': 'OK',
+#     }
 
-# client가 websocket이 연결되었는지 websocket으로 확인 받을 코드
-# 어떤 오류를 세워야하는지 애매(http or websocket)
-@user_router.get("/websocket/connect")
-async def client_check_connect(id : str, token : str):
-    if await check_token(id, token):
-        await manager.send_json(id, 'connected')
-        response = f'hi {id}, check your websocket response'
-    else:
-        response = f'Error {id}, check your id or token'
-    return {
-        'request': f'{PREFIX}/websocket/connect?id={id}',
-        'status': 'OK',
-        'response': response
-    }
+# # client가 websocket이 연결되었는지 websocket으로 확인 받을 코드
+# # 어떤 오류를 세워야하는지 애매(http or websocket)
+# @user_router.get("/websocket/connect")
+# async def client_check_connect(id : str, token : str):
+#     if await check_token(id, token):
+#         await manager.send_json(id, 'connected')
+#         response = f'hi {id}, check your websocket response'
+#     else:
+#         response = f'Error {id}, check your id or token'
+#     return {
+#         'request': f'{PREFIX}/websocket/connect?id={id}',
+#         'status': 'OK',
+#         'response': response
+#     }
