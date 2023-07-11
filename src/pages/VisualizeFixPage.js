bb173a94dc037a543904235ce4e80ace67b939eb
seo <deergirl11@gmail.com>
2023-07-11 23:32:13 +0900

    add visualize gaze-point code

--- src/pages/VisualizeFixPage.js
+++ src/pages/VisualizeFixPage.js
@@ -1,10 +1,86 @@
 import MenuBar from "../components/MenuBar";
+import VisualizeGaze from "../css/VisualizeGaze.module.css";
+import fixData from "../data/fixation data.json";
+import heatmap from "heatmap.js";
+
+import { useRef, useState, useEffect } from "react";
 
 function VisualizeFixPage() {
+  const canvasRef = useRef(null);
+  const canvasWidth = 285;
+  const [canvasHeight, setCanvasHeight] = useState(0);
+
+  useEffect(() => {
+    // heatmap.js 설정 및 초기화
+    const heatmapInstance = heatmap.create({
+      container: canvasRef.current,
+    });
+
+    const gazePoints = fixData[0].fixations.map((fixation) => fixation.gp);
+    const fixations = fixData[0].fixations;
+
+    console.log(gazePoints);
+
+    const heatmapData = processHeatmapData(fixations);
+    heatmapInstance.setData(heatmapData);
+
+    // 히트맵 렌더링하고 캔버스에 표시
+    heatmapInstance.repaint();
+    // 캔버스 높이 계산
+    calculateCanvasHeight(gazePoints);
+
+    console.log(canvasRef.current);
+  }, []);
+
+  //캔버스 높이 계산
+  const calculateCanvasHeight = (rawData) => {
+    console.log("height: ", rawData);
+    const minY = Math.min(...rawData.map((point) => point.y));
+    console.log("minY: ", minY);
+    const maxY = Math.max(...rawData.map((point) => point.y));
+    console.log("maxY: ", maxY);
+    const distance = maxY - minY;
+    const padding = distance * 0.2; // 20% 여백 추가
+    const calculatedHeight = distance + padding;
+
+    setCanvasHeight(calculatedHeight);
+  };
+
+  const processHeatmapData = (rawData) => {
+    console.log(rawData);
+    console.log(rawData[0]);
+
+    const heatmapData = {
+      max: 100,
+      data: [],
+    };
+
+    for (let i = 0; i < rawData.length - 1; i++) {
+      const { cx, cy, st, et, r } = rawData[i];
+      //   const nextT = rawData[i + 1].t;
+      //   const value = nextT - t; // timestamp의 간격
+      const adjustedX = (cx / canvasWidth) * 1000;
+      const adjustedY = (cy / canvasHeight) * 1000;
+      const value = r ** 2;
+
+      heatmapData.data.push({
+        x: adjustedX,
+        y: adjustedY,
+        value,
+      });
+    }
+
+    console.log(heatmapData);
+    return heatmapData;
+  };
+
   return (
     <div>
       <MenuBar />
-      <p>fixation 시각화 페이지</p>
+      <div>
+        <h3>store_list heatmap</h3>
+        <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} />
+      </div>
     </div>
   );
 }
--- src/pages/VisualizeGazePage.js
+++ src/pages/VisualizeGazePage.js
@@ -6,70 +6,67 @@ import heatmap from "heatmap.js";
 import { useRef, useState, useEffect } from "react";
 
 function VisualizeGazePage() {
-  const canvasRef = useRef(null);
-  const [canvasHeight, setCanvasHeight] = useState(0);
+  const [divHeights, setDivHeights] = useState([]);
+  const pages = ["store_list", "store_menu", "menu_detail", "cart"];
+  //   const [points, setPoints] = useState([]);
 
   useEffect(() => {
-    // heatmap.js 설정 및 초기화
-    const heatmapInstance = heatmap.create({
-      container: canvasRef.current,
+    const heights = pages.map((page) => {
+      const filteredData = gazeData.filter((item) => item.page === page);
+      return filteredData.length > 0
+        ? calculateHeight(filteredData[0].gaze)
+        : 0;
     });
 
-    const curUseData = gazeData[0].gaze;
-
-    const heatmapData = processHeatmapData(curUseData);
-    heatmapInstance.setData(heatmapData);
-
-    // 히트맵 렌더링하고 캔버스에 표시
-    heatmapInstance.repaint();
-    // 캔버스 높이 계산
-    calculateCanvasHeight(curUseData);
-
-    console.log(canvasRef.current);
+    setDivHeights(heights);
   }, []);
 
-  //캔버스 높이 계산
-  const calculateCanvasHeight = (rawData) => {
-    const minY = Math.min(...rawData.map((point) => point.y));
-    const maxY = Math.max(...rawData.map((point) => point.y));
+  const calculateHeight = (data) => {
+    if (!data || data.length === 0) return 0;
+
+    const minY = Math.min(...data.map((point) => point.y));
+    const maxY = Math.max(...data.map((point) => point.y));
     const distance = maxY - minY;
     const padding = distance * 0.2; // 20% 여백 추가
     const calculatedHeight = distance + padding;
 
-    setCanvasHeight(calculatedHeight);
-  };
-
-  const processHeatmapData = (rawData) => {
-    console.log(rawData);
-    console.log(rawData[0]);
-
-    const heatmapData = {
-      max: 1,
-      data: [],
-    };
-
-    for (let i = 0; i < rawData.length - 1; i++) {
-      const { x, y, t } = rawData[i];
-      const nextT = rawData[i + 1].t;
-      const value = nextT - t; // timestamp의 간격
-
-      heatmapData.data.push({
-        x,
-        y,
-        value,
-      });
-    }
-
-    console.log(heatmapData);
-    return heatmapData;
+    return calculatedHeight;
   };
 
   return (
     <div>
       <MenuBar />
       <div>
-        <h3>store_list heatmap</h3>
-        <canvas ref={canvasRef} width="285" height={canvasHeight} />
+        <h1>화면별 gaze-plot</h1>
+        <section className={VisualizeGaze.screens}>
+          {divHeights.map((divHeight, index) => {
+            const filteredGazeData = gazeData
+              .filter((item) => item.page === pages[index])
+              .flatMap((item) =>
+                item.gaze.filter((point) => point.x >= 0 && point.x <= 643.2)
+              );
+
+            return (
+              <div
+                key={index}
+                className={`${VisualizeGaze.gazePlot} ${VisualizeGaze.gazePlotRow}`}
+                style={{ height: divHeight }}
+              >
+                {filteredGazeData.map((point, idx) => (
+                  <div
+                    key={`${idx}`}
+                    className={VisualizeGaze.gazePoint}
+                    style={{
+                      left: point.x,
+                      top: point.y,
+                    }}
+                    title={pages[index]}
+                  ></div>
+                ))}
+              </div>
+            );
+          })}
+        </section>
       </div>
     </div>
   );
