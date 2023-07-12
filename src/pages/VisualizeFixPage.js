e8d2c77b5d7ffbf575fcfa7f77362bcdf31e210c
seo <deergirl11@gmail.com>
2023-07-13 00:21:52 +0900

    visualize fixations

--- src/pages/VisualizeFixPage.js
+++ src/pages/VisualizeFixPage.js
@@ -6,80 +6,74 @@ import heatmap from "heatmap.js";
 import { useRef, useState, useEffect } from "react";
 
 function VisualizeFixPage() {
-  const canvasRef = useRef(null);
-  const canvasWidth = 285;
-  const [canvasHeight, setCanvasHeight] = useState(0);
+  const [divHeights, setDivHeights] = useState([]);
+  const pages = ["store_list", "store_menu", "menu_detail", "cart", "order"];
 
   useEffect(() => {
-    // heatmap.js 설정 및 초기화
-    const heatmapInstance = heatmap.create({
-      container: canvasRef.current,
+    const heights = pages.map((page) => {
+      const filteredData = fixData.filter((item) => item.page === page);
+      return filteredData.length > 0
+        ? calculateHeight(filteredData[0].fixations)
+        : 0;
     });
 
-    const gazePoints = fixData[0].fixations.map((fixation) => fixation.gp);
-    const fixations = fixData[0].fixations;
-
-    console.log(gazePoints);
-
-    const heatmapData = processHeatmapData(fixations);
-    heatmapInstance.setData(heatmapData);
-
-    // 히트맵 렌더링하고 캔버스에 표시
-    heatmapInstance.repaint();
-    // 캔버스 높이 계산
-    calculateCanvasHeight(gazePoints);
-
-    console.log(canvasRef.current);
+    setDivHeights(heights);
   }, []);
 
-  //캔버스 높이 계산
-  const calculateCanvasHeight = (rawData) => {
-    console.log("height: ", rawData);
-    const minY = Math.min(...rawData.map((point) => point.y));
-    console.log("minY: ", minY);
-    const maxY = Math.max(...rawData.map((point) => point.y));
-    console.log("maxY: ", maxY);
+  const calculateHeight = (data) => {
+    if (!data || data.length === 0) return 0;
+
+    const minY = Math.min(
+      ...data.flatMap((fixation) => fixation.gp.map((point) => point.y))
+    );
+    const maxY = Math.max(
+      ...data.flatMap((fixation) => fixation.gp.map((point) => point.y))
+    );
     const distance = maxY - minY;
     const padding = distance * 0.2; // 20% 여백 추가
     const calculatedHeight = distance + padding;
 
-    setCanvasHeight(calculatedHeight);
-  };
-
-  const processHeatmapData = (rawData) => {
-    console.log(rawData);
-    console.log(rawData[0]);
-
-    const heatmapData = {
-      max: 100,
-      data: [],
-    };
-
-    for (let i = 0; i < rawData.length - 1; i++) {
-      const { cx, cy, st, et, r } = rawData[i];
-      //   const nextT = rawData[i + 1].t;
-      //   const value = nextT - t; // timestamp의 간격
-      const adjustedX = (cx / canvasWidth) * 1000;
-      const adjustedY = (cy / canvasHeight) * 1000;
-      const value = r ** 2;
-
-      heatmapData.data.push({
-        x: adjustedX,
-        y: adjustedY,
-        value,
-      });
-    }
-
-    console.log(heatmapData);
-    return heatmapData;
+    return calculatedHeight;
   };
 
   return (
     <div>
       <MenuBar />
       <div>
-        <h3>store_list heatmap</h3>
-        <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} />
+        <h1>화면별 fixations</h1>
+        <section className={VisualizeGaze.screens}>
+          {divHeights.map((divHeight, index) => {
+            const filteredFixData = fixData.filter(
+              (item) => item.page === pages[index]
+            );
+
+            return (
+              <div
+                key={index}
+                className={`${VisualizeGaze.gazePlot} ${VisualizeGaze.gazePlotRow}`}
+                style={{ height: divHeight }}
+              >
+                <div className={VisualizeGaze.gazeScreenName}>
+                  {pages[index]}
+                </div>
+                <svg width="643.2" height={divHeight}>
+                  {filteredFixData.flatMap((item) =>
+                    item.fixations.flatMap((fixation, fixationIdx) => (
+                      <circle
+                        key={`${fixationIdx}`}
+                        cx={fixation.cx}
+                        cy={fixation.cy}
+                        r={fixation.r}
+                        className={`${VisualizeGaze.fixCircle} ${VisualizeGaze.gazePoint}`}
+                        title={pages[index]}
+                      />
+                    ))
+                  )}
+                </svg>
+              </div>
+            );
+          })}
+        </section>
       </div>
     </div>
   );
