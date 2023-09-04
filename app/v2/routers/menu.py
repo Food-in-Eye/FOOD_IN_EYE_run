979641d3fe333c0b388cc7ecfa847dc6132c6d9e
mi642 <68195384+mi642@users.noreply.github.com>
2023-09-04 11:30:01 +0900

    modify - routers/menu.py -> customException, response 수정 완료

--- app/v2/routers/menu.py
+++ app/v2/routers/menu.py
@@ -5,13 +5,15 @@ menu_router
 import operator
 from datetime import datetime
 
-from fastapi import APIRouter, Depends, Request, HTTPException
+from fastapi import APIRouter, Depends, Request
 from core.models.store import MenuModel
+from core.common.authority import TokenManagement
 from core.common.mongo import MongodbController
+from core.error.exception import CustomException
 from .src.util import Util
 from .src.meta import Meta
 
-from core.common.authority import TokenManagement
+
 TokenManager = TokenManagement()
 
 menu_router = APIRouter(prefix="/menus", dependencies=[Depends(TokenManager.dispatch)])
@@ -27,128 +29,82 @@ async def hello():
 @menu_router.get("/q")
 async def read_menus_of_store(s_id:str, request:Request):
     """ 주어진 가게 아이디의 모든 메뉴판들을 불러온다. """
+    assert TokenManager.is_seller(request), 403.1
 
-    assert TokenManager.is_buyer(request), "This request is only allowed for buyers."
+    Util.check_id(s_id)
+    response = DB.read_all('menu', {'s_id': s_id}) 
 
-    try:
-        Util.check_id(s_id)
-        response = DB.read_all('menu', {'s_id': s_id}) 
-
-    except Exception as e:
-        print('ERROR', e)
-        return {
-            'request': f'GET {PREFIX}?s_id={s_id}',
-            'status': 'ERROR',
-            'message': f'ERROR {e}'
-        }
-    
     return {
-        'request': f'GET {PREFIX}?s_id={s_id}',
-        'status': 'OK',
         'response': response
     }
 
 @menu_router.get("/menu")
 async def read_menu(id:str):
     """ 해당하는 id의 menu 정보를 받아온다. """
-    try:
-        _id = Util.check_id(id)
-        response = DB.read_one('menu', {'_id':_id})
 
-    except Exception as e:
-        print('ERROR', e)
-        return {
-            'request': f'GET {PREFIX}?id={id}',
-            'status': 'ERROR',
-            'message': f'ERROR {e}'
-        }
+    _id = Util.check_id(id)
+    response = DB.read_one('menu', {'_id':_id})
     
     return {
-        'request': f'GET {PREFIX}?id={id}',
-        'status': 'OK',
-        'response': response
+        "_id": id,
+        "s_id": response['store_id'],
+        "date": response['date'],
+        "f_list" : response['f_list']
     }
 
 @menu_router.post("/menu")
 async def create_menu(s_id:str, menu:MenuModel, request:Request):
     """ 해당하는 가게에 새로운 메뉴판을 설정한다. """
-
-    assert TokenManager.is_seller(request), "This request is only allowed for sellers."
-
+    assert TokenManager.is_seller(request), 403.1
+    
     data = menu.dict()
     
-    try:
-        _id = Util.check_id(s_id)
+    _id = Util.check_id(s_id)
 
-        new_menu = {
-            's_id': s_id,
-            'date': datetime.now(),
-            'f_list': data['f_list']
-        }
+    new_menu = {
+        's_id': s_id,
+        'date': datetime.now(),
+        'f_list': data['f_list']
+    }
 
-        new_id = str(DB.insert_one('menu', new_menu))
+    new_id = str(DB.insert_one('menu', new_menu))
 
-        if DB.update_one('store', {'_id': _id}, {'m_id': new_id}):
-            update_meta(s_id, new_id)
-            return {
-                'request': f'POST {PREFIX}/menu?s_id={s_id}',
-                'status': 'OK'
-            }
-        
-        else:
-            return {
-            'request': f'POST {PREFIX}/menu?s_id={s_id}',
-            'status': 'ERROR',
-            'message': f'ERROR!! Please contact your administrator'
-        }
-
-    except Exception as e:
-        print('ERROR', e)
-        return {
-            'request': f'POST {PREFIX}/menu?s_id={s_id}',
-            'status': 'ERROR',
-            'message': f'ERROR {e}'
-        }
+    if DB.update_one('store', {'_id': _id}, {'m_id': new_id}) == False:
+        update_meta(s_id, new_id)
+        raise CustomException(503.53)
+    
 
 @menu_router.get('/menu/foods')
 async def read_menu_with_foods(id:str):  
     """ 해당하는 id의 음식 정보를 포함한 메뉴판 정보를 받아온다. """
 
-    try:
-        _id = Util.check_id(id)
-        response = DB.read_one('menu', {'_id':_id})
-        
-        
-        food_ids = [food['f_id'] for food in response['f_list']]
-        food_list = []
+    _id = Util.check_id(id)
+    response = DB.read_one('menu', {'_id':_id})
+    
+    
+    food_ids = [food['f_id'] for food in response['f_list']]
+    food_list = []
 
-        for f_id in food_ids:
-            _id = Util.check_id(f_id)
-            food = DB.read_one('food', {'_id': _id})
-            food_list.append({
-                "f_id": f_id,
-                "name": food['name'],
-                "price": food['price'],
-                "img_key" : food['img_key'],
-                "desc" : food['desc'],
-                "allergy" : food['allergy'],
-                "origin" : food['origin'],
-                "num": food['num']
-            })
-        response['f_list'] = food_list
-
-    except Exception as e:
-        print('ERROR', e)
-        return {
-            'request': f'GET {PREFIX}/menu/foods?id={id}',
-            'status': 'ERROR',
-            'message': f'ERROR {e}'
-        }
+    for f_id in food_ids:
+        _id = Util.check_id(f_id)
+        food = DB.read_one('food', {'_id': _id})
+        food_list.append({
+            "f_id": f_id,
+            "name": food['name'],
+            "price": food['price'],
+            "img_key" : food['img_key'],
+            "desc" : food['desc'],
+            "allergy" : food['allergy'],
+            "origin" : food['origin'],
+            "num": food['num']
+        })
+    response['f_list'] = food_list
     
     return {
-        'request': f'GET {PREFIX}/menu/foods?id={id}',
-        'status': 'OK',
-        'response': response
+        "_id": id,
+        "s_id": response['store_id'],
+        "date": response['date'],
+        "f_list" : response['f_list']
     }
 
 def update_meta(s_id:str, new_m_id:str):
