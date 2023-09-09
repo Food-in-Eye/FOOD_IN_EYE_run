fccea90b288757ff2cca6eb67ed263a7ce19a125
mi642 <68195384+mi642@users.noreply.github.com>
2023-09-09 19:52:27 +0900

    modify - v2/routers/src/util.py -> get_custim_time() 함수 추가

--- app/v2/routers/src/util.py
+++ app/v2/routers/src/util.py
@@ -31,3 +31,11 @@ class Util:
         current_time = current_time_utc.astimezone(pytz.timezone('Asia/Seoul'))
         return current_time
     
+    @staticmethod
+    def get_custom_time(date:datetime) -> datetime:
+        """ 
+            datetime을 입력 받아 원하는 timezone으로 현재 시간을 리턴한다. 
+            - input : datetime 객체(year, month, day)
+        """
+        custom_time = date.astimezone(pytz.timezone('Asia/Seoul'))
+        return custom_time
