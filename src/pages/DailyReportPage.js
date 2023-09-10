1bb7be537fd0896e6cec17963e38696b4e8e7265
seo <deergirl11@gmail.com>
2023-09-10 10:09:58 +0900

    add auto-move func using useRef

--- src/pages/DailyReportPage.js
+++ src/pages/DailyReportPage.js
@@ -1,14 +1,52 @@
 import MenuBar from "../components/MenuBar";
-import Total from "../css/DailyReport.module.css";
+import DR from "../css/DailyReport.module.css";
+
+import { useRef } from "react";
 
 function DailyReportPage() {
+  const useMoveScroll = (elementId) => {
+    const element = useRef(null);
+    const onMoveToElement = () => {
+      element.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+    };
+    return { element, onMoveToElement, name: elementId };
+  };
+
+  const tabs = {
+    0: useMoveScroll("오늘의 리포트"),
+    1: useMoveScroll("시간당 주문량 및 시선 수"),
+    2: useMoveScroll("시선/체류 시간과 주문량"),
+    3: useMoveScroll("메뉴별 방문 시선 수"),
+    length: 4,
+  };
+
   return (
     <div>
       <section className="header">
         <MenuBar />
       </section>
-      <div className={Total.inner}>
-        <p>전체 메뉴 분석 페이지</p>
+
+      {Array.from(tabs).map((tab, index) => (
+        <div
+          key={index}
+          className={DR.tabsToClick}
+          onClick={tab.onMoveToElement}
+        >
+          {tab.name}
+        </div>
+      ))}
+
+      <div ref={tabs[0].element} className={DR.tabElement}>
+        <h2>오늘의 리포트</h2>
+      </div>
+      <div ref={tabs[1].element} className={DR.tabElement}>
+        <h2>시간당 주문량 및 시선 수</h2>
+      </div>
+      <div ref={tabs[2].element} className={DR.tabElement}>
+        <h2>시선/체류 시간과 주문량</h2>
+      </div>
+      <div ref={tabs[3].element} className={DR.tabElement}>
+        <h2>메뉴별 방문 시선 수</h2>
       </div>
     </div>
   );
