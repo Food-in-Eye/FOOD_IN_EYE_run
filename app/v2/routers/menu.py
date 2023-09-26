521d81d3cb9dbfec08272b3b87e0ed3d2064b9a7
MiRa <emira1239@naver.com>
2023-09-26 17:20:10 +0900

    routers/menu.py - 사용하지 않는 import 삭제

--- app/v2/routers/menu.py
+++ app/v2/routers/menu.py
@@ -9,7 +9,6 @@ from fastapi import APIRouter, Depends, Request
 from core.models.store import MenuModel
 from core.common.authority import TokenManagement
 from core.common.mongo import MongodbController
-from core.error.exception import CustomException
 from .src.util import Util
 from .src.meta import Meta
 
